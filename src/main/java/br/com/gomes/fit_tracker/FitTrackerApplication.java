ff4b0cb7ddd436b20f84b3866c911f719bbed2fb
Isaque Gomes <isaquegeovani29@gmail.com>
2026-04-14 11:38:31 -0300

    chore: initial commit and project structure

---
+++ src/main/java/br/com/gomes/fit_tracker/FitTrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gomes.fit_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gomes/fit_tracker/FitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gomes.fit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
